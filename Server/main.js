8c5ad2ef6e362cd6059498b1aed17e4ba2190dab
Oliver Högström <oliver.hogstrom@elev.ga.ntig.se>
2020-09-01 10:01:56 +0200

    Update main.js

--- Server/main.js
+++ Server/main.js
@@ -1,4 +1,4 @@
 let name = "Oliver Högström"
 let year = 17
 
-console.log("My name is " + name + "and I am " + year + " years old.")
+console.log("My name is " + name + " and I am " + year + " years old.")
