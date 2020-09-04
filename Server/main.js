b57184a3586787eecf013af4b8ca8a9214d2ff3c
Oliver Högström <oliver.hogstrom@elev.ga.ntig.se>
2020-09-04 15:24:08 +0200

    Did some extra on this fucker

--- Server/main.js
+++ Server/main.js
@@ -1,14 +1,18 @@
-let name = "Oliver Högström"
-let shit = "You are not even 18 you little shit"
-let Yay = "You can now buy beer at the bar!"
+let name = "Oliver"
 let age = 17
+let shit = "You are not Oliver you little shit!!!"
+let Yay = "You may pass Master Oliver!"
 
-if( age <= 17){
-    console.log(shit)
-}
-else if(age >= 17 && age <=25){
+
+if( age == 17 && name === "Oliver"){
     console.log(Yay)
 }
+else if(age != 17 && name === "Oliver"){
+    console.log(shit)
+}
+else if(age == 17 && name != "Oliver"){
+    console.log(shit)
+}
 else{
-    console.log("Do what ever you want!")
+    console.log("You dont belong here!")
 }
