914a3fac6dcab37284f019f8cc664196eceb9843
Oliver Högström <oliver.hogstrom@elev.ga.ntig.se>
2020-10-09 13:23:39 +0200

    Started with Dice game

---
+++ Dice game/app.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+// 1. Skriv in in 4 tal, varav 2 är dice 1 och de andra 2 är dice 2
+rl.question("Write the span of the dices: "), (answer) =>{
+    // skapa variabler för avr 1 och avrage 2
+
+    let splitAnswer = answer.spilt(' ')
+    
+
+}
