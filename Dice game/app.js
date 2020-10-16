4d8d48b7c950f8ef8e3fc0923ada4b59d5c3bc44
oliverhogstrom <54627509+oliverhogstrom@users.noreply.github.com>
2020-10-16 12:05:11 +0200

    Now im done with dice game
    Its not pretty i know, but the first puch was wrong due to the calculations being wrong, The solution i came up with is not nice at all, but it gets the calculations right,

--- Dice game/app.js
+++ Dice game/app.js
@@ -14,13 +14,19 @@ function code(){
     let side2 = Number(lines[1])
     let side3 = Number(lines[2])
     let side4 = Number(lines[3])
+    let side5 = Number(lines[4])
+    let side6 = Number(lines[5])
+    let side7 = Number(lines[6])
+    let side8 = Number(lines[7])
 
 // Emma is result1 and gunnar is result 2
 
-    let result1 = (side1 + side2)/2
-    let result2 = (side3 + side4)/2
 
-    console.log(result1 , result2);
+
+    let result1 = ((side1 + side2)/2) + ((side3 + side4)/2)
+    let result2 = ((side5 + side6)/2) + ((side7 + side8)/2)
+
+    console.log(result1, ":", result2);
 
     if(result1>result2){
       console.log('Emma is the winner with a score of: ' + result1)
@@ -33,7 +39,7 @@ function code(){
 
   rl.on('line', (input) => {
     lines.push(input)
-    if(lines.length == 4){
+    if(lines.length == 8){
       code()
       rl.close()
     }
