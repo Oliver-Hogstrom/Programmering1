f85451303377e02dd8984367cb50282ab0ae27e4
Oliver Högström <oliver.hogstrom@elev.ga.ntig.se>
2020-11-06 12:30:26 +0100

    Small change
    I replaced pareseint with Number(), cause i dont know what parseint is...

--- guess/main.js
+++ guess/main.js
@@ -1,37 +1,33 @@
-const readline = require ('readline')
+const readline = require('readline')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
-let random = Math.round(Math.random() * 100) + 1;
-  
-let tries = 5
+let random = Math.round(Math.random() * 10) + 1
 
-console.log("You have to guess the right number, it's a number between 1-100")
+let tries = 8
 
-      rl.on('line', (input)=>{
-        let ans = parseInt(input)
-        tries--;
+console.log("You have to guess the right number, it's a number between 1-10 and you have:", tries, "tries")
 
-        if(tries === 0){
-          console.log("You're out of tries m8 :(")
-          console.log("The number was: ", random);
-          process.exit();
-        }
-        else if(random==ans){
-          
-          console.log("Congrats, you've won!");
-          process.exit();
-        }
-        else if(random>ans){
-          console.log("You have ", tries, " tries left");
-          console.log("You have to guess higher mate!");
-        }
-        else if(random<ans){
-          console.log("You have ", tries, "tries left");
-          console.log("You have to guess lower mate!");
-        } 
-        })
-      
+rl.on('line', (input) => {
+    let ans = Number(input)
+    tries--;
+
+    if (tries === 0) {
+        console.log("You're out of tries m8 :(")
+        console.log("The number was: ", random);
+        process.exit();
+    } else if (random == ans) {
+
+        console.log("Congrats, you've won!");
+        process.exit();
+    } else if (random > ans) {
+        console.log("You have ", tries, " tries left");
+        console.log("You have to guess higher mate!");
+    } else if (random < ans) {
+        console.log("You have ", tries, "tries left");
+        console.log("You have to guess lower mate!");
+    }
+})
