05ef2a3fd4b7d41f1f6083b1073b9eeb6511fd5d
Oliver Högström <oliver.hogstrom@elev.ga.ntig.se>
2020-12-01 09:34:02 +0100

    done with random

--- Hangman/Instructions.md
+++ Hangman/Instructions.md
@@ -1,18 +1,22 @@
-###Hangman
+#Hangman
 <br>
 // 1. Ge användaren instruktioner om hur spelet ska gå till med console.log()
 <br>
-// 2. Slumpa ett ord från en lista som användaren ska gissa på med en random funktion()
+// 2. Slumpa ett ord från en lista som användaren ska gissa på med en random funktion() använder mig av math.floor och en array för att slumpa ett ord
 <br>
 //     2.1 Slumpa eller be användaren att skriva in ett eget ord som andra ska gissa på (Optional)
 <br>
-//     3.1 be användare att skriva in ett ord i listan med random ord, en push till listan.
+//     2.2 be användare att skriva in ett ord i listan med random ord, en push till listan.
 <br>
 // 3. Be användaren gissa 7 gånger på en bokstav åt gången
 <br>
-//     3.1 Skapa villkor ifall användaren gissar med ett tal eller flera bokstäver.
+//     3.1 Skapa en loop (While eller for) för att behålla spelarens gissnigs process till dem antingen inte har några gissningar kvar eller har gissat rätt ord.
+<br>
+//     3.2 Skapa villkor ifall användaren gissar med ett tal eller flera bokstäver i loopen.
 <br>
 // 4. Spara användarens gissningar och skriv ut dem i console i den ordning som orden finns i orden
+<br>
+//      4.1 skriv även ut i en console log hur många fel gissningar som spelaren har kvar efter varje gissnins.
 <br>
 // 5. Skapa villkor för om användaren vinner eller när användaren har slut på gissningar aka dödat mannen
 <br>
--- Hangman/app.js
+++ Hangman/app.js
@@ -3,17 +3,19 @@
 // 1. Ge användaren instruktioner om hur spelet ska gå till med console.log()
 // 2. Slumpa ett ord från en lista som användaren ska gissa på med en random funktion()
 //     2.1 Slumpa eller be användaren att skriva in ett eget ord som andra ska gissa på (Optional)
-//     3.1 be användare att skriva in ett ord i listan med random ord, en push till listan.
+//     2.1 be användare att skriva in ett ord i listan med random ord, en push till listan.
 // 3. Be användaren gissa 7 gånger på en bokstav åt gången
-//     3.1 Skapa villkor ifall användaren gissar med ett tal eller flera bokstäver.
+//     3.1 Skapa en loop (While eller for) för att behålla spelarens gissnigs process till dem antingen inte har några gissningar kvar eller har gissat rätt ord.
+//     3.2 Skapa villkor ifall användaren gissar med ett tal eller flera bokstäver i loopen.
 // 4. Spara användarens gissningar och skriv ut dem i console i den ordning som orden finns i orden
+//      4.1 skriv även ut i en console log hur många fel gissningar som spelaren har kvar efter varje gissnins.
 // 5. Skapa villkor för om användaren vinner eller när användaren har slut på gissningar aka dödat mannen
 
 // Vad som alltid ska skrivas ut: Skriv ut vilka bokstäver som användaren har gissat på
 
 // När jag byggt klart spelet, implementera att måla ut gubben i konsollen, eller bind ihop spelet med en .HTML
 
-// Let the games begin
+// Let the games begin 
 
 const readline = require('readline')
 
@@ -32,3 +34,23 @@ function intruductionToPlayer() {
 }
 
 // This section will be dedicated to the words that will be randomized from a array of different words
+// I have a array with words i have written myself and a random function to randomize the words in the array
+
+let hangmanWords = ['discord', 'programming', 'server', 'node', 'wallah']
+
+function randomWords() {
+    
+    let randomHangmanWords = hangmanWords[Math.floor(hangmanWords.length * Math.random())]
+
+    let splittedWord = randomHangmanWords.split('')
+    console.log(splittedWord);
+}
+
+// Now we need to ask the player to guess and compere the guesses with the splitted random word.
+function playerGuess() {
+    const tries = 7
+
+    if (condition) {
+        
+    }
+}
